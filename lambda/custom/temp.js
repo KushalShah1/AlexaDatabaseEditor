16b78a0be02ed38935fd01887f2a7fdb2c49f1f2
Kushal Shah <kushalshah@flash.net>
2019-11-18 21:52:37 -0600

    fixed some privacy issues so had to remove version history

---
+++ lambda/custom/temp.js
@@ -0,0 +1,59 @@
+
+var mysql = require('mysql');
+var db = require('./modules/configDatabase.json')
+
+var con = mysql.createConnection({
+    host: db.host,
+    port: db.port,
+    user: db.user,
+    password: db.password
+});
+
+var dbEdit=require('./modules/databaseManager.js');
+
+let sql = "INSERT INTO Item VALUES ('4','test','test from test.js')";
+function addToDbw(sql) {
+    return new Promise((resolve, reject) =>
+        con.connect(function (err) {
+            if (err) throw err;
+            con.query("USE test", function (err, result) {
+                if (err) throw err;
+                con.query(sql, function (err, result) {
+                    if (err) {
+                        reject("There was an error adding");
+                    }
+                    else {
+                        resolve("Add was successful");
+                    }
+                });
+
+            })
+        }))
+}
+
+/*addToDb(sql)
+    .then(value=>{
+        con.end();
+        console.log(value);
+    })
+    .catch(err => {
+        con.end();
+        console.log(err)
+    });
+let sql = "INSERT INTO Item VALUES ('2','test','test from test.js');";
+
+let speakOutput = 'Add was '
+con.query("Select * Item;", function (err, result) {
+    if (err) throw err;
+    speakOutput += result;
+    console.log(speakOutput);
+});
+*/
+dbEdit.addToDb(sql)
+            .then(value => {
+                console.log(value);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+return 0;
